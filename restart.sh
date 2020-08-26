1ed960379d59158890ecb06b2d4b55e90a43fdf8
ixiaofeng <ixiaofeng.com@gmail.com>
2020-08-26 12:08:00 +0800

    optimize

--- restart.sh
+++ restart.sh
@@ -2,5 +2,5 @@
 #ps -ef|grep client|awk '{print $2}'|xargs kill -9
 #ulimit -n 100000
 #php /www/wwwroot/phpDhtSpider/dht_client/go.php
-rm -rf /www/wwwroot/phpDhtSpider/dht_client/logs/*
-rm -rf /www/wwwroot/phpDhtSpider/dht_server/logs/*
+rm -rf /www/wwwroot/crazyDhtSpider/dht_client/logs/*
+rm -rf /www/wwwroot/crazyDhtSpider/dht_server/logs/*
