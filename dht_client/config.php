608bff4a15522a17696c83acc30b9218c023aab9
ixiaofeng <ixiaofeng.com@gmail.com>
2020-10-15 17:18:53 +0800

    optimize

--- dht_client/config.php
+++ dht_client/config.php
@@ -2,7 +2,7 @@
 return array(
     'daemonize' => true,//是否后台守护进程
     'worker_num' => 4,// 主进程数, 一般为CPU的1至4倍 同时执行任务数量
-    'task_worker_num' => 150,//task进程的数量 值越大 CPU占用越高
+    'task_worker_num' => 300,//task进程的数量 值越大 CPU占用越高
     'server_ip' => '127.0.0.1',//服务端ip
     'server_port' => 2345,//服务端端口
     'max_conn' => 65535,//最大连接数
--- dht_client/go.php
+++ dht_client/go.php
@@ -9,7 +9,7 @@ ini_set('date.timezone', 'Asia/Shanghai');
 ini_set("memory_limit", "-1");
 define('BASEPATH', dirname(__FILE__));
 $config = require_once BASEPATH . '/config.php';
-define('MAX_REQUEST', 0);// worker 进程的最大任务数,根据自己的实际情况设置
+define('MAX_REQUEST', 0);// worker 进程的最大任务数,先不要设置，否则cpu很快100%。
 define('AUTO_FIND_TIME', 1000);//定时寻找节点时间间隔 /毫秒
 define('MAX_NODE_SIZE', 1000);//保存node_id最大数量,不要设置太大，否则会导致数组过大内存溢出
 define('BIG_ENDIAN', pack('L', 1) === pack('N', 1));
@@ -109,7 +109,7 @@ $serv->on('task', function ($server, Swoole\Server\Task $task) {
         }
         $client->close(true);
     }
-
+    co::sleep(1);
     $task->finish("OK");
 });
 
