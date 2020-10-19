b480e8fefb3ef3a4cf9ad26b6a2a548ca28e0b81
ixiaofeng <ixiaofeng.com@gmail.com>
2020-10-19 21:21:24 +0800

    optimize check_cpu.sh

--- check_cpu.sh
+++ check_cpu.sh
@@ -3,12 +3,14 @@
 #while (i--)
 #do
 pid=`ps aux|grep dht_client_task|sort -rn -k3|head -1|awk '{print $2}'`
-cpu=`ps -p $pid -o pcpu |egrep -v CPU`
+cpu=`top -bn1 -n 1  -p $pid | tail -1 | awk '{printf("%d",$9)}' | sed 's/ /,/'`
 echo $cpu
 echo $pid
-if [ $cpu > 95 ]
+if [ "$cpu" -gt "95" ]
 then
 kill -9 $pid;
 echo "kill success";
+else
+echo "no need to kill";
 fi
 #done
