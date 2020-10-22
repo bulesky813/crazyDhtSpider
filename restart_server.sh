5affcef24c563348283769dcccf7222d89fb80d4
ixiaofeng <ixiaofeng.com@gmail.com>
2020-10-22 23:59:31 +0800

    Daily optimization

--- restart_server.sh
+++ restart_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ps -ef|grep dht_server|awk '{print $2}'|xargs kill -9
-ulimit -n 100000
+ulimit -n 65535
 rm -rf /www/wwwroot/crazyDhtSpider/dht_server/logs/*
 php /www/wwwroot/crazyDhtSpider/dht_server/go.php
