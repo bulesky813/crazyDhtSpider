9e3b78e5dfbd9ca0d5878366ade979abc3535028
ixiaofeng <ixiaofeng.com@gmail.com>
2020-11-02 15:57:26 +0800

    Daily optimization

--- dht_server/go.php
+++ dht_server/go.php
@@ -1,8 +1,7 @@
 <?php
-//swoole version 1.9.5
+//swoole version 4.0+
 /*
- * 安装swoole pecl install swoole
- * 设置服务器 ulimit -n 100000
+ * 设置服务器 ulimit -n 65535
  * 记得放开防火墙6882端口
  */
 error_reporting(E_ERROR);
@@ -17,7 +16,6 @@ require_once BASEPATH . '/inc/Db.class.php';
 
 //记录启动日志
 Func::Logs(date('Y-m-d H:i:s', time()) . " - 服务启动..." . PHP_EOL, 1);
-swoole_set_process_name("php_dht_server:[master] worker");
 
 //SWOOLE_PROCESS 使用进程模式，业务代码在Worker进程中执行
 //SWOOLE_SOCK_UDP 创建udp socket
@@ -25,56 +23,13 @@ $serv = new Swoole\Server('0.0.0.0', 2345, SWOOLE_PROCESS, SWOOLE_SOCK_UDP);
 $serv->set($config);
 
 $serv->on('WorkerStart', function ($serv, $worker_id) use ($config) {
+    swoole_set_process_name("php_dht_server_event_worker");
     Db::$config = array(
         'host' => $config['db']['host'],
         'user' => $config['db']['user'],
         'pass' => $config['db']['pass'],
         'name' => $config['db']['name'],
     );
-
-    swoole_set_process_name("php_dht_server:[" . $worker_id . "] worker");
-});
-
-$serv->on('Receive', function ($serv, $fd, $from_id, $data) {
-    if (strlen($data) == 0) {
-        $serv->close($fd, true);
-        return false;
-    }
-    //$fdinfo = $serv->connection_info($fd, $from_id);
-    $rs = Base::decode($data);
-    if (is_array($rs) && isset($rs['infohash'])) {
-        $data = Db::get_one("select 1 from history where infohash = '$rs[infohash]' limit 1");
-        if (!$data) {
-            Db::insert('history', array('infohash' => $rs['infohash']));
-            $files = '';
-            $length = 0;
-            if ($rs['files'] != '') {
-                $files = json_encode($rs['files'], JSON_UNESCAPED_UNICODE);
-                foreach ($rs['files'] as $value) {
-                    $length += $value['length'];
-                }
-            } else {
-                $length = $rs['length'];
-            }
-            Db::insert('bt', array(
-                    'name' => $rs['name'],
-                    'keywords' => Func::getKeyWords($rs['name']),
-                    'infohash' => $rs['infohash'],
-                    'files' => $files,
-                    'length' => $length,
-                    'piece_length' => $rs['piece_length'],
-                    'hits' => 0,
-                    'time' => date('Y-m-d H:i:s'),
-                    'lasttime' => date('Y-m-d H:i:s'),
-                )
-            );
-        } else {
-            $files = addslashes(json_encode($rs['files'], JSON_UNESCAPED_UNICODE));
-            $last_time=date('Y-m-d H:i:s');
-            Db::query("update bt set `hot` = `hot` + 1, `lasttime`= '".$last_time."',`files` = '" . $files . "' where infohash = '$rs[infohash]'");
-        }
-    }
-    $serv->close($fd, true);
 });
 
 $serv->on('Packet', function ($serv, $data, $clientInfo) {
@@ -82,9 +37,15 @@ $serv->on('Packet', function ($serv, $data, $clientInfo) {
         $serv->close(true);
         return false;
     }
-    //$fdinfo = $serv->connection_info($fd, $from_id);
     $rs = Base::decode($data);
     if (is_array($rs) && isset($rs['infohash'])) {
+        /*$redis = new Redis();
+        $redis->connect('127.0.0.1', 6379);
+        $redis->auth('fuck@lidebo');
+        $redis->select(10);
+        $redis_data = $redis->sIsMember('infohash',$rs['infohash']);
+        if(!$redis_data){
+            $redis->sAdd('infohash',$rs['infohash']);*/
         $data = Db::get_one("select 1 from history where infohash = '$rs[infohash]' limit 1");
         if (!$data) {
             Db::insert('history', array('infohash' => $rs['infohash']));
@@ -112,10 +73,20 @@ $serv->on('Packet', function ($serv, $data, $clientInfo) {
             );
         } else {
             $files = addslashes(json_encode($rs['files'], JSON_UNESCAPED_UNICODE));
-            $last_time=date('Y-m-d H:i:s');
-            Db::query("update bt set `hot` = `hot` + 1, `lasttime`= '".$last_time."',`files` = '" . $files . "' where infohash = '$rs[infohash]'");
+            $last_time = date('Y-m-d H:i:s');
+            Db::query("update bt set `hot` = `hot` + 1, `lasttime`= '" . $last_time . "',`files` = '" . $files . "' where `infohash` = '$rs[infohash]'");
         }
+        /*}else{
+            $files = addslashes(json_encode($rs['files'], JSON_UNESCAPED_UNICODE));
+            $last_time = date('Y-m-d H:i:s');
+            Db::query("update bt set `hot` = `hot` + 1, `lasttime`= '" . $last_time . "',`files` = '" . $files . "' where `infohash` = '$rs[infohash]'");
+        }*/
     }
     $serv->close(true);
 });
+
+$serv->on('WorkerExit', function ($server, $worker_id) {
+    Swoole\Timer::clearAll();
+});
+
 $serv->start();
